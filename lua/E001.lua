182a86741084b28eb16017ff3a35c37654e5ee13
Jim Fingal <jim.fingal@gmail.com>
2014-12-15 18:03:07 -0800

    ha -- for look just as good

--- lua/E001.lua
+++ lua/E001.lua
@@ -1,9 +1,11 @@
+var helpers = require('./helpers')
+
 var solution = function() {
 
     let max = 1000;
     let sum = 0;
 
-    for (let x = 0; x < 1000; x++) { 
+    for (let x of helpers.ascendingRangeGen(0, 1000, 1)) {
         if (x % 3 == 0 || x % 5 == 0) {
             sum += x;
         }
